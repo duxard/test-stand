874be12f9a73386b96d09952e5bf5d7d74627bb2
Aleksandr <duxard@gmail.com>
2019-11-01 12:03:38 -0400

    started to work on Comp1

--- src/components/Comp1.js
+++ src/components/Comp1.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Comp1 = () => {
   return (
     <div>
-      <h1>Component 1</h1>
+      <div class="row">
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s12">
+              <input placeholder="Placeholder" id="email" type="email" class="validate" />
+              <label for="email">Email</label>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
