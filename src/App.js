afbe6ca3272ac0b49b1729f128f3e47c10174cd1
Aleksandr <duxard@gmail.com>
2019-11-01 11:50:55 -0400

    init point

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Comp1 from './components/Comp1';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+
+import './index.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/create' component={Comp1} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/comp1' component={Comp1} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to='/' className="brand-logo">Home</Link>
+        <ul className="right">
+          <li><NavLink to='/signup'>Sign up</NavLink></li>
+          <li><NavLink to='/signin'>Sign in</NavLink></li>
+          <li><NavLink to='/comp1'>Comp1</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
