90fad6162e0c145d331c5c282a25785b4af9c991
duxard <duxard@gmail.com>
2019-11-09 12:16:19 +0300

    make list items responsive

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,8 +3,14 @@ import React from 'react';
 const ListItem = props => {
   return (
     <li key={props.id} data-itemid={props.dataItemId}>
-      <span>{props.item}</span>
-      <button className="btn" onClick={props.parentHandleDelete}>Delete</button>
+      <div className="trow">
+        <div className="tleft">
+          <p>{props.item}</p>
+        </div>
+        <div className="tright">
+          <button className="btn deleteBtn" onClick={props.parentHandleDelete}>Delete</button>
+        </div>
+      </div>
     </li>
   )
 }
